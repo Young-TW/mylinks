513d606232cbf076380fe4878604073ee1e2cf16
young <young20050727@gmail.com>
2023-04-28 11:32:33 +0800

    update imgs style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,19 @@
 import Link_Cards from './link_cards'
-import Image from "next/image";
+import Image from "next/legacy/image";
 
 export default function Home() {
   return (
     <main>
       <div className="mx-auto h-48">
-        <Image
-          src="/Young.jpg"
-          alt="avator"
-          width={36}
-          height={36}
-          className="w-36 h-36 rounded-full mx-auto p-4"
-        />
+        <div className="w-36 h-36 mx-auto p-4">
+          <Image
+            src="/Young.jpg"
+            alt="avator"
+            width={120}
+            height={120}
+            className="rounded-full"
+          />
+        </div>
         <p className="text-center text-2xl font-bold mx-4">
           Hi, I am Young. Here are some of my links.
         </p>
