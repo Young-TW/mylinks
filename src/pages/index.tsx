155e893ff5620016884ba18b0a064c3a8860b456
young <young20050727@gmail.com>
2023-05-03 12:17:52 +0800

    add soundcloud music

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import Link_Cards from './link_cards'
 import Image from "next/legacy/image";
+import Music from './music';
 
 export default function Home() {
   return (
     <main>
-      <div className="mx-auto h-48">
-        <div className="w-36 h-36 mx-auto p-4">
+      <div className="mx-auto h-fit p-4">
+        <div className="w-36 h-36 mx-auto">
           <Image
             src="/Young.jpg"
             alt="avator"
@@ -14,11 +15,14 @@ export default function Home() {
             className="rounded-full"
           />
         </div>
-        <p className="text-center text-2xl font-bold mx-4">
+        <p className="text-center text-xl font-bold mx-4">
           Hi, I am Young. Here are some of my links.
         </p>
       </div>
-      <div className="w-3/4 container mx-auto">
+      <div className="h-32 mx-6 flex justify-center">
+        <Music />
+      </div>
+      <div className="mt-6 w-3/4 container mx-auto">
         <Link_Cards />
       </div>
     </main>
---
+++ src/pages/music.tsx
@@ -0,0 +1,7 @@
+export default function Music() {
+  return (
+    <div className="w-3/4">
+      <iframe width="100%" height="166" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1323742591&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+    </div>
+  )
+}
