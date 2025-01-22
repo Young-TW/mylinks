350969efb797364e6b91546d921eef2d87fd6ea0
Young <young20050727@gmail.com>
2025-01-22 13:49:38 +0800

    update links

--- src/pages/link_cards.tsx
+++ src/pages/link_cards.tsx
@@ -2,69 +2,69 @@ import Image from "next/legacy/image";
 
 const links = [
   {
-    title: 'Blog',
-    subtitle: '🔗 My blog!',
-    link: 'https://blog.young-tw.com',
+    title: "Blog",
+    subtitle: "🔗 My blog!",
+    link: "https://blog.young-tw.com",
     bg: "/browser.png",
   },
   {
-    title: 'Matrix',
-    subtitle: '@-young-:matrix.org | 🏡 Come to chat with me!',
-    link: 'https://matrix.to/#/@-young-:matrix.org',
+    title: "Matrix",
+    subtitle: "@-young-:matrix.org | 🏡 Come to chat with me!",
+    link: "https://matrix.youngtw.net",
     bg: "/matrix.jpg",
   },
   {
-    title: 'Uptime Kuma',
-    subtitle: '📈 All of my websites uptime status',
-    link: 'http://status.young-tw.com:12345/status/public',
+    title: "Uptime Kuma",
+    subtitle: "📈 All of my websites uptime status",
+    link: "http://status.young-tw.com:12345/status/public",
     bg: "/uptimekuma.png",
   },
   {
-    title: 'GitHub',
-    subtitle: '@Young-TW | 📽️ All my open-sourced projects here',
-    link: 'https://github.com/Young-TW',
+    title: "GitHub",
+    subtitle: "@Young-TW | 📽️ All my open-sourced projects here",
+    link: "https://github.youngtw.net",
     bg: "/github.png",
   },
   {
-    title: 'Mastodon.social',
-    subtitle: '@young_tw | 🏡 My Mastodon.social account',
-    link: 'https://mastodon.social/@young_tw',
+    title: "Mastodon.social",
+    subtitle: "@young_tw | 🏡 My Mastodon.social account",
+    link: "https://mastodon.youngtw.net",
     bg: "/mastodon.jpg",
   },
   {
-    title: 'g0v.social',
-    subtitle: '@young | 🏡 My Taiwanese g0v Mastodon account',
-    link: 'https://g0v.social/@young',
+    title: "g0v.social",
+    subtitle: "@young | 🏡 My Taiwanese g0v Mastodon account",
+    link: "https://g0v.youngtw.net",
     bg: "/mastodon.jpg",
   },
   {
-    title: 'YouTube',
-    subtitle: '🎬 Find my videos here!',
-    link: 'https://www.youtube.com/@young-tw',
+    title: "YouTube",
+    subtitle: "🎬 Find my videos here!",
+    link: "https://www.youtube.com/@young-tw",
     bg: "/youtube.png",
   },
   {
-    title: 'Telegram',
-    subtitle: '@young_tw | 🏡 DM with me',
-    link: 'https://t.me/young_tw',
+    title: "Telegram",
+    subtitle: "@young_tw | 🏡 DM with me",
+    link: "https://telegram.youngtw.net",
     bg: "/telegram.png",
   },
   {
-    title: 'osu!',
-    subtitle: '@-Young- | 🎮 My osu! profile',
-    link: 'https://osu.ppy.sh/users/15561507',
+    title: "osu!",
+    subtitle: "@-Young- | 🎮 My osu! profile",
+    link: "https://osu.ppy.sh/users/15561507",
     bg: "/osu.jpeg",
   },
   {
-    title: 'Bandcamp',
-    subtitle: '@youngtw | 🎵 Come to find out my favorite music',
-    link: 'https://bandcamp.com/youngtw',
+    title: "Bandcamp",
+    subtitle: "@youngtw | 🎵 Come to find out my favorite music",
+    link: "https://bandcamp.com/youngtw",
     bg: "/bandcamp.png",
   },
   {
-    title: 'Steam',
-    subtitle: '@youngtw | 🎮 Play games with me',
-    link: 'https://steamcommunity.com/id/youngtw/',
+    title: "Steam",
+    subtitle: "@youngtw | 🎮 Play games with me",
+    link: "https://steamcommunity.com/id/youngtw/",
     bg: "/steam.jpeg",
   },
 ];
@@ -74,9 +74,15 @@ export default function LinkCards() {
     <div className="flex flex-wrap">
       {links.map((p) => {
         return (
-          <div className="h-48 w-64 mt-6 rounded-lg mx-auto hover:animate-pulse" key={p.title}>
+          <div
+            className="h-48 w-64 mt-6 rounded-lg mx-auto hover:animate-pulse"
+            key={p.title}
+          >
             <a href={p.link}>
-              <div className="w-fit h-1/2 rounded-lg" style={{width: '100%', height: '50%', position: 'relative'}}>
+              <div
+                className="w-fit h-1/2 rounded-lg"
+                style={{ width: "100%", height: "50%", position: "relative" }}
+              >
                 <Image
                   src={p.bg}
                   layout="fill"
@@ -89,14 +95,12 @@ export default function LinkCards() {
                 <h3 className="notosans text-center text-lg h-fit">
                   {p.title}
                 </h3>
-                <p className="text-sm px-4">
-                  {p.subtitle}
-                </p>
+                <p className="text-sm px-4">{p.subtitle}</p>
               </div>
             </a>
           </div>
         );
       })}
     </div>
-  )
+  );
 }
