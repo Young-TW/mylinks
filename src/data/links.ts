71ba7bf1c2107c1574b5db03dae803785d054558
young <young20050727@gmail.com>
2025-03-29 10:07:44 +0800

    refactor: move links info into data dir

---
+++ src/data/links.ts
@@ -0,0 +1,62 @@
+export const links = [
+  {
+      title: "Blog",
+      subtitle: "🔗 My blog!",
+      link: "https://blog.young-tw.com",
+      bg: "/browser.png",
+  },
+  {
+      title: "Matrix",
+      subtitle: "@-young-:matrix.org | 🏡 Come to chat with me!",
+      link: "https://matrix.youngtw.net",
+      bg: "/matrix.jpg",
+  },
+  {
+      title: "Uptime Kuma",
+      subtitle: "📈 All of my websites uptime status",
+      link: "http://status.young-tw.com:12345/status/public",
+      bg: "/uptimekuma.png",
+  },
+  {
+      title: "Mastodon.social",
+      subtitle: "@young_tw | 🏡 My Mastodon.social account",
+      link: "https://mastodon.youngtw.net",
+      bg: "/mastodon.jpg",
+  },
+  {
+      title: "g0v.social",
+      subtitle: "@young | 🏡 My Taiwanese g0v Mastodon account",
+      link: "https://g0v.youngtw.net",
+      bg: "/mastodon.jpg",
+  },
+  {
+      title: "YouTube",
+      subtitle: "🎬 Find my videos here!",
+      link: "https://www.youtube.com/@young-tw",
+      bg: "/youtube.png",
+  },
+  {
+      title: "Telegram",
+      subtitle: "@young_tw | 🏡 DM with me",
+      link: "https://telegram.youngtw.net",
+      bg: "/telegram.png",
+  },
+  {
+      title: "Quaver",
+      subtitle: "@young0727 | 🎮 My Quaver profile",
+      link: "https://quavergame.com/user/236859",
+      bg: "/quaver.jpg",
+  },
+  {
+      title: "Bandcamp",
+      subtitle: "@youngtw | 🎵 Come to find out my favorite music",
+      link: "https://bandcamp.com/youngtw",
+      bg: "/bandcamp.png",
+  },
+  {
+      title: "Steam",
+      subtitle: "@youngtw | 🎮 Play games with me",
+      link: "https://steamcommunity.com/id/youngtw/",
+      bg: "/steam.jpeg",
+  },
+];
--- src/pages/link_cards.tsx
+++ src/pages/link_cards.tsx
@@ -1,75 +1,7 @@
 import Image from "next/legacy/image";
 import OsuCard from "./osu_card";
-import { github_stats } from "./github_card";
-
-const links = [
-  {
-    title: "Blog",
-    subtitle: "🔗 My blog!",
-    link: "https://blog.young-tw.com",
-    bg: "/browser.png",
-  },
-  {
-    title: "Matrix",
-    subtitle: "@-young-:matrix.org | 🏡 Come to chat with me!",
-    link: "https://matrix.youngtw.net",
-    bg: "/matrix.jpg",
-  },
-  {
-    title: "Uptime Kuma",
-    subtitle: "📈 All of my websites uptime status",
-    link: "http://status.young-tw.com:12345/status/public",
-    bg: "/uptimekuma.png",
-  },
-  /* {
-    title: "GitHub",
-    subtitle: "@Young-TW | 📽️ All my open-sourced projects here",
-    link: "https://github.youngtw.net",
-    bg: "/github.png",
-  }, */
-  {
-    title: "Mastodon.social",
-    subtitle: "@young_tw | 🏡 My Mastodon.social account",
-    link: "https://mastodon.youngtw.net",
-    bg: "/mastodon.jpg",
-  },
-  {
-    title: "g0v.social",
-    subtitle: "@young | 🏡 My Taiwanese g0v Mastodon account",
-    link: "https://g0v.youngtw.net",
-    bg: "/mastodon.jpg",
-  },
-  {
-    title: "YouTube",
-    subtitle: "🎬 Find my videos here!",
-    link: "https://www.youtube.com/@young-tw",
-    bg: "/youtube.png",
-  },
-  {
-    title: "Telegram",
-    subtitle: "@young_tw | 🏡 DM with me",
-    link: "https://telegram.youngtw.net",
-    bg: "/telegram.png",
-  },
-  {
-    title: "Quaver",
-    subtitle: "@young0727 | 🎮 My Quaver profile",
-    link: "https://quavergame.com/user/236859",
-    bg: "/quaver.jpg",
-  },
-  {
-    title: "Bandcamp",
-    subtitle: "@youngtw | 🎵 Come to find out my favorite music",
-    link: "https://bandcamp.com/youngtw",
-    bg: "/bandcamp.png",
-  },
-  {
-    title: "Steam",
-    subtitle: "@youngtw | 🎮 Play games with me",
-    link: "https://steamcommunity.com/id/youngtw/",
-    bg: "/steam.jpeg",
-  },
-];
+import { github_stats } from "../data/github_card";
+import { links } from "../data/links";
 
 export default function LinkCards() {
   return (
