e6df9ba89b21c83bd87bf9c829d4aa1613339a58
Young <young20050727@gmail.com>
2025-07-30 11:20:28 +0800

    feat: add link_cards

---
+++ src/pages/link_cards.svelte
@@ -0,0 +1,34 @@
+<script>
+  import OsuCard from './osu_card.svelte';
+  import QuaverCard from './quaver_card.svelte';
+  import GithubCard from './github_card.svelte';
+  import { link_cards } from '../data/links.js';
+</script>
+
+<style>
+  @import '../styles/link_cards.css';
+</style>
+
+<div class="linkcards-flex">
+  <OsuCard />
+  <QuaverCard />
+  <GithubCard />
+
+  {#each link_cards as p (p.title)}
+    <div class="linkcard">
+      <a href={p.link}>
+        <div class="linkcard-img-wrapper">
+          <img
+            src={p.bg}
+            alt={p.title}
+            class="linkcard-img"
+          />
+        </div>
+        <div class="linkcard-info">
+          <h3 class="linkcard-title">{p.title}</h3>
+          <p class="linkcard-subtitle">{p.subtitle}</p>
+        </div>
+      </a>
+    </div>
+  {/each}
+</div>
