35293826a822507623099e6d7e838a871ab3fa86
Young <young20050727@gmail.com>
2025-07-30 11:12:32 +0800

    feat: add github_cards

---
+++ src/pages/github_card.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { github_stats } from '../data/github_card.js';
+</script>
+
+<style>
+  @import '../styles/github_card.css';
+</style>
+
+<div class="github-cards">
+  {#each github_stats as p (p.alt)}
+    <div class="github-card">
+      <a href="https://github.youn.gg" title="GitHub">
+        <img
+          src={p.src}
+          height={p.height}
+          width={p.width}
+          alt={p.alt}
+          class="linkcard-img"
+        />
+      </a>
+    </div>
+  {/each}
+</div>
